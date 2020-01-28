19ed2b08c8954c8f2dc1e9dcc1c459ad6bcc5218
amirbigg <amirbig44@gmail.com>
2020-01-28 21:37:01 +0330

    add .

--- first/views.py
+++ first/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic.base import TemplateView
+from .models import Todo
 
 
-class Home(View):
-	def get(self, request, *args, **kwargs):
-		return render(request, 'first/home.html', {'name':'Mongard'})
+class Home(TemplateView):
+	template_name = 'first/home.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['todos'] = Todo.objects.all()
+		return context
