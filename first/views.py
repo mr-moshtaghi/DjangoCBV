1383e96bf694fa7166c8469507a594b3fca5aa94
amirbigg <amirbig44@gmail.com>
2020-01-31 20:33:37 +0330

    add .

--- first/views.py
+++ first/views.py
@@ -1,12 +1,23 @@
 from .models import Todo
-from django.views.generic.list import ListView
+from django.views.generic import ListView # __init__.py   __all__
+from django.views.generic import DetailView
 
 
 class Home(ListView): # first/todo_list.html
 	template_name = 'first/home.html'
-	context_object_name = 'todos'
+	context_object_name = 'todos' # object_list
 	ordering = ['-created']
 
 	def get_queryset(self):
 		return Todo.objects.all()
 
+
+class DetailTodo(DetailView): # first/todo_detail.html  object
+	slug_field = 'slug'
+	slug_url_kwarg = 'myslug'
+
+	def get_queryset(self, **kwargs):
+		if self.request.user.is_authenticated:
+			return Todo.objects.filter(slug=self.kwargs['myslug'])
+		else:
+			return Todo.objects.none()
