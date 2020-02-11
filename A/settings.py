ce62c4c87a95cde1ad70a5a72802501dc5ae3987
amirbigg <amirbig44@gmail.com>
2020-02-11 12:33:31 +0330

    add .

--- A/settings.py
+++ A/settings.py
@@ -102,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LOGIN_REDIRECT_URL = 'first:home'
+LOGOUT_REDIRECT_URL = 'first:home'
 
 
 # Internationalization
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
 	path('login/', views.UserLogin.as_view(), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
