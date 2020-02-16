6616faceb0004f7cf888f6be1f4735d95eba8646
amirbigg <amirbig44@gmail.com>
2020-02-16 21:08:12 +0330

    add .

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
 	path('login/', views.UserLogin.as_view(), name='login'),
 	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 	path('reset/', views.UserPassReset.as_view(), name='reset_pass'),
-	path('reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/reset_done.html'), name='password_reset_done'),
+	path('reset/done/', views.PasswordResetDone.as_view(), name='password_reset_done'),
+	path('confirm/<uidb64>/<token>/', views.PasswordResetConfirm.as_view(), name='password_reset_confirm'),
+	path('confirm/done/', views.PasswordResetComplete.as_view(), name='password_reset_complete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -9,4 +9,14 @@ class UserLogin(auth_views.LoginView):
 class UserPassReset(auth_views.PasswordResetView):
 	template_name = 'accounts/password_reset_form.html'
 	success_url = reverse_lazy('accounts:password_reset_done')
-	html_email_template_name = 'accounts/password_reset_email.html'
+	email_template_name = 'accounts/password_reset_email.html'
+
+class PasswordResetDone(auth_views.PasswordResetDoneView):
+	template_name = 'accounts/reset_done.html'
+
+class PasswordResetConfirm(auth_views.PasswordResetConfirmView):
+	template_name = 'accounts/password_reset_confirm.html'
+	success_url = reverse_lazy('accounts:password_reset_complete')
+
+class PasswordResetComplete(auth_views.PasswordResetCompleteView):
+	template_name = 'accounts/password_reset_complete.html'
