450bd0f2eaa547cf85fc644a6604b261036be8d4
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2021-03-08 15:23:20 +0330

    update

--- accounts/views.py
+++ accounts/views.py
@@ -4,19 +4,23 @@ from django.urls import reverse_lazy
 
 
 class UserLogin(auth_views.LoginView):
-	template_name = 'accounts/login.html'
+    template_name = 'accounts/login.html'
+
 
 class UserPassReset(auth_views.PasswordResetView):
-	template_name = 'accounts/password_reset_form.html'
-	success_url = reverse_lazy('accounts:password_reset_done')
-	email_template_name = 'accounts/password_reset_email.html'
+    template_name = 'accounts/password_reset_form.html'
+    success_url = reverse_lazy('accounts:password_reset_done')
+    email_template_name = 'accounts/password_reset_email.html'
+
 
 class PasswordResetDone(auth_views.PasswordResetDoneView):
-	template_name = 'accounts/reset_done.html'
+    template_name = 'accounts/reset_done.html'
+
 
 class PasswordResetConfirm(auth_views.PasswordResetConfirmView):
-	template_name = 'accounts/password_reset_confirm.html'
-	success_url = reverse_lazy('accounts:password_reset_complete')
+    template_name = 'accounts/password_reset_confirm.html'
+    success_url = reverse_lazy('accounts:password_reset_complete')
+
 
 class PasswordResetComplete(auth_views.PasswordResetCompleteView):
-	template_name = 'accounts/password_reset_complete.html'
+    template_name = 'accounts/password_reset_complete.html'
