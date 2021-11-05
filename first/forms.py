22c3af10af9cf188e014b21a9b4ce7775e2039e3
mr-moshtaghi <mr.ahmadalimoshtaghi@gamil.com>
2021-11-05 13:52:13 +0330

    update

--- first/forms.py
+++ first/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from .models import Comment
 
+
 class TodoCommentForm(forms.ModelForm):
-	class Meta:
-		model = Comment
-		fields = ('name', 'body')
+    class Meta:
+        model = Comment
+        fields = ('name', 'body')
+
+
+class TodoCreateForm(forms.Form):
+    title = forms.CharField(max_length=200)
